0515a97dee3ba3d32e5c930d4b4628889fa06594
MrigankaArya <mrig.arya@gmail.com>
2017-01-20 23:29:12 -0800

    Attached light to player object

---
+++ Assets/lightbehaviourscript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightbehaviourscript : MonoBehaviour {
+
+// Use this for initialization
+public GameObject player;
+public Vector3 lightPos = new Vector3(0, 0, 0);
+void Start () {
+player = GameObject.FindGameObjectWithTag("player1");
+lightPos.x = player.transform.position.x;
+lightPos.y = player.transform.position.y;
+lightPos.z = player.transform.position.z;
+    this.transform.position = lightPos;
+
+}
+	
+// Update is called once per frame
+void Update () {
+        lightPos.x = player.transform.position.x;
+        lightPos.y = player.transform.position.y;
+        this.transform.position = lightPos;
+		
+}
+}
