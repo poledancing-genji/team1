239336bd31cf233ae05631741b1a6efa15aeb4d4
Maxim Tsai <Maxim Tsai>
2017-01-20 18:09:05 -0800

    the big initial commit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")) {
+            Debug.Log(this.transform);
+            this.transform.position = new Vector3(this.transform.position.x + 0.1f, this.transform.position.y, 0);
+            // rigidbody2D.velocity = new Vector2(movex * Speed, movey * Speed);
+        }
+	}
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# GGJGame
+unity game for the global game jam
+
+Github by Maxim Tsai.
+
+General Instructions:
+git pull                         - downloads all updates other people made to your computer
+git add .                        - adds all the stuff you did so it's ready to be committed
+git commit -m "mymessagehere"    - commits your work so it's ready to be pushed.
+git push
