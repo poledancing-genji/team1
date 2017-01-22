486bb288530c9bb5d90146d7a3b2206c8c5efa11
Jacob <madridjacobr@gmail.com>
2017-01-22 13:31:37 -0800

    Modified music file to play all tracks in sequence

---
+++ Assets/MusicHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicHandler : MonoBehaviour {
+
+    public AudioSource[] musicTracks;
+    public int currentIndex = 0;
+	// Update is called once per frame
+
+    void Awake()
+    {
+        musicTracks[currentIndex].Play();
+    }
+	void Update () {
+		// get player health
+        if (!musicTracks[currentIndex].isPlaying) {
+            if (currentIndex < 2)
+            {
+                currentIndex += 1;
+            } else
+            {
+                currentIndex = 0;
+            }
+            musicTracks[currentIndex].Play();
+        }
+        
+	}
+}
