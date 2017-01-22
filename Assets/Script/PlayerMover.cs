60118f32fa88e63da07241f8bae9b41e4b42d294
Jacob <madridjacobr@gmail.com>
2017-01-22 01:22:27 -0800

    simplified player moving

---
+++ Assets/Script/PlayerMover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+    public Rigidbody playerBody;
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+    public float speed;
+    public float angularSpeed;
+
+    void FixedUpdate() {
+       if (Input.GetKey(up))
+        {
+            Vector3 forward = speed * transform.forward;
+            playerBody.AddForce(forward);
+        } else if (Input.GetKey(down))
+        {
+            Vector3 backward = -1 * speed * transform.forward;
+            playerBody.AddForce(backward);
+        }
+
+        if (Input.GetKey(right))
+        {
+            transform.Rotate(Vector3.up * angularSpeed);
+        }
+        else if (Input.GetKey(left))
+        {
+            transform.Rotate(-1 * Vector3.up * angularSpeed);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+       if (collision.gameObject.tag == "wall")
+       {
+            collision.gameObject.GetComponent<thewallsarealsogay>().ActivateWall();
+        }
+    }
+}
