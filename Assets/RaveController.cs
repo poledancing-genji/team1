26d90ba487ab27c14aaa424d1df47343f75ff623
MrigankaArya <mrig.arya@gmail.com>
2017-01-21 18:00:41 -0800

    made lights work and add models

---
+++ Assets/RaveController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaveController : MonoBehaviour {
+    public Color[] colorArray;
+    public int i;
+    private int numColors;
+    public Color currentColor;
+    // Use this for initialization
+    void Start () {
+        numColors = colorArray.Length;
+        IEnumerator coroutine = WaitAndGay(0.5f);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator WaitAndGay(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            currentColor = colorArray[i];
+            i++;
+            Debug.Log(currentColor);
+            i = i % numColors;
+        }
+    }
+}
---
+++ Assets/thewallsarealsogay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thewallsarealsogay : MonoBehaviour {
+    public MeshRenderer wallRenderer;
+    private int countInsideTrigger = 0;
+    private Color groundColor;
+    public RaveController r;
+    void Start()
+    {
+        //groundColor = GameObject.Find("Ground").GetComponent<MeshRenderer>().materials[0].GetColor("_Color");
+        groundColor = Color.black;
+        Debug.Log(groundColor);
+        r = GameObject.Find("Raver").GetComponent<RaveController>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trig");
+        countInsideTrigger++;
+    }
+
+    void Update ()
+    {
+        Debug.Log(countInsideTrigger);
+        if (countInsideTrigger > 0) { 
+            wallRenderer.materials[0].SetColor("_LineColor", r.currentColor);
+        } else
+        {
+            wallRenderer.materials[0].SetColor("_LineColor", groundColor);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("asdf");
+        countInsideTrigger--;
+    }
+}
